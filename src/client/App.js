7b8e14d8878ee65d55e5d38733b54ec1effbb7b2
sharonKerz <sharonkerz@gmail.com>
2018-10-12 23:50:51 +0300

    adding un attached MatchMakerBuilder

--- src/client/App.js
+++ src/client/App.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout'
 import EventsBuilder from './containers/EventsBuilder/EventsBuilder'
 import CompanyProfileBuilder from './containers/CompanyProfileBuilder/CompanyProfileBuilder'
+import MatchMakerBuilder from './containers/MatchMakerBuilder/MatchMakerBuilder'
 import { Route } from 'react-router-dom'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>
           <Route path="/" exact component={EventsBuilder} />
-          <Route path="/Companies" component={CompanyProfileBuilder}/>
+          <Route path="/Companies" component={CompanyProfileBuilder} />
+          <Route path="/MatchMaker" component={CompanyProfileBuilder} />
         </Layout>
       </div>
     );
---
+++ src/client/containers/MatchMakerBuilder/MatchMakerBuilder.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import axios from '../../axios/axios-beasy'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import {withRouter} from 'react-router-dom';
+
+import Wrapper from '../../hoc/Wrapper/Wrapper'
+import Events from '../../components/Events/Events'
+import CenterHorizontalGrid from '../../ui/Grid/CenterHorizontalGrid'
+import Modal from '../../ui/Modal/Modal'
+import EventSummery from '../../components/Events/EventSummery'
+import AddButton from '../../components/Buttons/AddButton/AddButton'
+
+class MatchMakerBuilder extends Component {
+    state = {
+        openEventModal: false,
+        events: [],
+        selectedEvent: {}
+    }
+
+    componentDidMount() {
+        axios.get('/events').then(response => {
+            this.setState({ events: response.data })
+            console.log(response);
+        });
+    }
+
+    openEventModalHandle = (event) => {
+        this.setState({ selectedEvent: event });
+        this.setState({ openEventModal: true });
+    };
+
+    closeEventModalHandle = () => {
+        this.setState({ openEventModal: false });
+    };
+
+    addEventHandler = () => {
+        const event = {
+            companyName: "Generated Company",
+            title: "Generated Title",
+            date: "Generated Date",
+            body: "GeneratedBody",
+            id: 100
+        }
+        axios.post('/eventssss', event)
+            .then(response => console.log(response))
+            .catch(error => this.openErrorModalHandle(error.message))
+    };
+
+    render() {
+        return (
+            <Wrapper>
+                <Modal isOpen={this.state.openEventModal} closeModal={this.closeEventModalHandle}>
+                    <EventSummery event={this.state.selectedEvent} />
+                </Modal>
+                <div>
+                    <h2>Events</h2>
+                    <AddButton onClick={this.addEventHandler}></AddButton>
+                </div>
+                <div>
+                    <CenterHorizontalGrid>
+                        <Events events={this.state.events}
+                            openEventModal={this.openEventModalHandle} />
+                    </CenterHorizontalGrid>
+                </div>
+            </Wrapper>
+        );
+    }
+}
+
+export default withRouter(withErrorHandler(MatchMakerBuilder, axios));
